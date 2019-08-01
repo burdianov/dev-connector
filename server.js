f07d507177b8e1ab233a058d2833fd9d68421111
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-08-01 21:48:50 +0400

    request and body validation

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+// Init Middleware
+// allow using the data in req.body by applying this middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('API Running');
 });
